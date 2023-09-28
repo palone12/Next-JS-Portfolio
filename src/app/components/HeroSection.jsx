e2424ef6227a9ae18fe49c5164228970b452a46d
Pranay Alone <64677840+palone12@users.noreply.github.com>
2023-09-29 01:01:48 +0530

    Changed Linke URL

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,13 +44,13 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/contact"
+              href="https://www.linkedin.com/in/pranayalone/"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/1cfZr6dy5rm1PEkbd42dzmp7t9u5Gn1MC/view?usp=drive_link"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
